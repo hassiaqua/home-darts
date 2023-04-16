1f70933656fba9f94b6362caaf6c810b2a74dbb0
橋爪@grooweb <hashizume@grooweb.jp>
2023-04-16 20:43:36 +0900

    no message

--- html/js/common.js
+++ html/js/common.js
@@ -72,29 +72,130 @@ function scrollBlocker(flag){
 // 	}
 // });
 
-/* sp menu
+/* calc
 *********************************************/
-let hit_number = 0;
-let hit_multiple = 0;
-let hit_score = 0;
 
-let countup_score = 0;
+/* ダーツ入力 */
+function input() {
 
-$('.js-trigger').click(function() {
-	hit_number = $(this).data('number');
-	hit_multiple = $(this).data('multiple');
-	hit_score = hit_number * hit_multiple;
+	$('.js-trigger').click(function() {
 
-	// console.log(hit_score);
+		/* number,multipleを取得 */
+		let hit_number = $(this).data('number');
+		let	hit_multiple = $(this).data('multiple');
 
-	$('.test-throw').append('<p class="test-throw__item">'+hit_score+'</p>');
+		/* 点数計算 */
+		let hit_score = 0;
+		if (hit_number === 'bull') { //bullの場合
+			if (hit_multiple == '1') {
+				hit_score = 50;
+			} else if(hit_multiple == '2'){
+				hit_score = 50;
+			}
+		} else { //bull以外の場合
+			hit_score = hit_number * hit_multiple;
+		}
 
-	countup_score += hit_score;
-	console.log(countup_score);
-	$('.test-countup').html(countup_score)
+		countup(hit_score);
+
+	});
+}
+input();
+
+/* カウントアップ */
+
+let countup_array = [];
+function countup(score) {
+
+	countup_array.push(score);
+
+	// console.log(score);
+	// console.log(countup_array);
 
 
-});
+}
+
+	var score = 0;
+  var round = 1;
+  var scores = [];
+
+  function updateProgress() {
+    var progress = '';
+    for (var i = 0; i < 8; i++) {
+      if (scores[i] === undefined) {
+        progress += '-';
+      } else {
+        progress += scores[i].reduce(function(a, b) {
+          return a + b;
+        }, 0);
+      }
+      progress += ' ';
+    }
+    $('#progress').text(progress);
+  }
+
+  $('.js-trigger').click(function() {
+    var points = parseInt($(this).data('number'));
+    score += points;
+    $('#score').text(score);
+
+    if (scores[round - 1] === undefined) {
+      scores[round - 1] = [];
+    }
+    scores[round - 1].push(points);
+
+    if (scores[round - 1].length === 3) {
+      round += 1;
+      updateProgress();
+    }
+
+    if (round > 8) {
+      // 計算結果を出力する
+      var totalScore = 0;
+      for (var i = 0; i < 8; i++) {
+        var roundScore = scores[i].reduce(function(a, b) {
+          return a + b;
+        }, 0);
+        totalScore += roundScore;
+        console.log('Round ' + (i + 1) + ': ' + roundScore);
+      }
+      console.log('Total Score: ' + totalScore);
+
+      // ゲームをリセットする
+      score = 0;
+      round = 1;
+      scores = [];
+      $('#score').text(score);
+      $('#progress').text('');
+    }
+  });
+
+
+
+
+
+
+// let hit_number = 0;
+// let hit_multiple = 0;
+// let hit_score = 0;
+
+// let countup_score = 0;
+
+// $('.js-trigger').click(function() {
+// 	hit_number = $(this).data('number');
+// 	hit_multiple = $(this).data('multiple');
+// 	hit_score = hit_number * hit_multiple;
+
+// 	// console.log(hit_score);
+
+// 	$('.test-throw').append('<p class="test-throw__item">'+hit_score+'</p>');
+
+// 	countup_score += hit_score;
+// 	console.log(countup_score);
+// 	$('.test-countup').html(countup_score)
+
+
+// });
 
 /********************************************
  [PC ONLY]
